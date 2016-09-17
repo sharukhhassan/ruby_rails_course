ab1090932af38d9485aeb824c7f25b1c5a910db9
Sharukh Hassan <sharukh_hassan@live.com>
2016-09-17 12:34:55 +0530

    Initial Commit

---
+++ module2_lesson1_formative_case.rb
@@ -0,0 +1,12 @@
+some_var="false"
+another_var=nil
+puts case
+when some_var=="pink elephant"
+	"Dont think about elephant!"
+when another_var.nil?
+	"Question mark in the method name?"
+when some_var==false
+	"Looks like this shoud exeute"
+else
+	"I guess nothing matched.. But why?"
+end
